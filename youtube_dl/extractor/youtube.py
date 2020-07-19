453f701f9b53e19ee94c65664edadc617c4bcfbb
barbaragribeiro <barbaragomes0@hotmail.com>
2020-07-18 21:22:07 -0300

    Fixing like_count and dislike_count

--- youtube_dl/extractor/youtube.py
+++ youtube_dl/extractor/youtube.py
@@ -2374,7 +2374,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
 
         def _extract_count(count_name):
             return str_to_int(self._search_regex(
-                r'-%s-button[^>]+><span[^>]+class="yt-uix-button-content"[^>]*>([\d,]+)</span>'
+                r'([0-9,]+) %ss"}}'
                 % re.escape(count_name),
                 video_webpage, count_name, default=None))
 
